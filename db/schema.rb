2e819409f6290a7bfcd39b53268a3de923dba883
Christopher <chris.changchien@gmail.com>
2014-10-06 21:02:27 +0000

    began installing/configuring devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002014442) do
-
-  create_table "comments", force: true do |t|
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+ActiveRecord::Schema.define(version: 20141002014226) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
