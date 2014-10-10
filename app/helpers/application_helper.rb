0c740e3d47d80803ff0c00a9f896a6705b7c950c
Christopher <chris.changchien@gmail.com>
2014-10-10 22:21:21 +0000

    branch for different style of generating div tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 module ApplicationHelper
-  def form_group_tag(errors, &block)
+  def form_group_tag(errors)
     if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
-    else
-      content_tag :div, capture(&block), class: 'form-group'
+      'has-error'
     end
   end
 end
