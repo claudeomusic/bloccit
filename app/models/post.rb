aaa34d5062c2dcb42381121d7c42ea93d10ff8eb
Christopher <chris.changchien@gmail.com>
2014-10-17 21:59:25 +0000

    added voting functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,31 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
 
   mount_uploader :image, ImageUploader
 
-  default_scope {order('created_at DESC')}
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+  after_create :create_vote
+
+  default_scope {order('rank DESC')}
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  #validates :topic, presence: true
+  #validates :user, presence: true
 
   def markdown_title
     render_as_markdown title
@@ -18,6 +33,12 @@ class Post < ActiveRecord::Base
 
   def markdown_body
     render_as_markdown body
+  end
+
+  def update_rank
+      age = (created_at - Time.new(1970,1,1)) / (60*60*24)
+      new_rank = points + age
+      update_attribute(:rank, new_rank)
   end
 
   private
@@ -28,6 +49,11 @@ class Post < ActiveRecord::Base
     redcarpet = Redcarpet::Markdown.new(renderer,extensions)
     (redcarpet.render text).html_safe
   end
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
 
 
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,8 +1,15 @@
+require 'rails_helper'
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect (@vote.value).to eq(-1 || 1)
+        v = Vote.new(value: 1)
+        v2 = Vote.new(value: -1)
+        bad_v = Vote.new(value: 2)
+        expect(v.valid?).to eq(true)
+        expect(v2.valid?).to eq(true)
+        expect(bad_v.valid?).to eq(false)
       end
     end
   end
